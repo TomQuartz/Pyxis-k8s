68b7f854707fe345e71890a093d963f417cd2df0
TomQuartz <shengqi2018@pku.edu.cn>
2024-11-28 02:29:02 -0700

    fix env expansion in pod command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,28 +2,45 @@
 
 BASE_DIR=`realpath $(dirname $0)`
 ROOT_DIR=$BASE_DIR/..
+cd $ROOT_DIR
 
 set -ex
 
+# Usage: compute|storage num_nodes workers_per_node
 function deploy_server {
-    kubectl apply -f $ROOT_DIR/manifests/server.yaml
+    server=$1
+    replicas=$2
+    workers=$3
+    # update the configmap
+    kubectl delete configmap $server-config --ignore-not-found
+    kubectl create configmap $server-config \
+        --from-literal=WORKERS=$workers
+    # delete to force reload the configmap
+    kubectl delete -f $ROOT_DIR/manifests/$server.yaml --ignore-not-found
+    kubectl apply -f $ROOT_DIR/manifests/$server.yaml
+    # scale the deployment
+    kubectl scale deployment pyxis-$server --replicas=$replicas
 }
 
-# Usage: client -arbiter=pyxis|kayak -maxout=8|16|32...
+# Usage: client [-debug] -arbiter=pyxis|kayak -maxout=8|16|32...
 function deploy_client {
     go run $ROOT_DIR/cmd/client/main.go $@
 }
 
 case $1 in
-server)
-    deploy_server
+compute|storage)
+    deploy_server $@
+    ;;
+clean)
+    kubectl delete -f $ROOT_DIR/manifests/compute.yaml --ignore-not-found
+    kubectl delete -f $ROOT_DIR/manifests/storage.yaml --ignore-not-found
     ;;
 client)
     shift
     deploy_client $@
     ;;
 *)
-    echo "Usage: $0 {server|client}"
+    echo "Usage: $0 {compute|storage|client}"
     exit 1
     ;;
 esac
